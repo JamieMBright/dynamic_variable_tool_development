bbba3a9c9034cb2b06624c1799cf40e5f541b2d0
Jamie Bright <jamie.bright@anu.edu.au>
2018-03-14 14:57:21 +1100

    altered the switch setup to instead feed each of the raw datasources a flag of wheter to produce a variable or not, when MODIS is called, it will loop and produce all variables requested of it. The output of this will feed into a processing stage that is separated by each out_variable

--- DynamicVariableProductionTool.m
+++ DynamicVariableProductionTool.m
@@ -1,20 +1,20 @@
 % Dynamic Variable Production Wrapper
 %
 % ========================================================================
-%                               Information 
+%                               Information
 % ========================================================================
 % Started: 13/03/2018
 % Author: Dr Jamie M. Bright
-% 
+%
 % Affiliation: The Australian National University, Fenner School.
 % License: Open source, must cite.
-% 
-% Prefered Citation: Bright, J.M. et al. 2018. Global clear-sky irradiance 
-% model validation using dynamic variables from NCEP, MODIS and OMI. 
+%
+% Prefered Citation: Bright, J.M. et al. 2018. Global clear-sky irradiance
+% model validation using dynamic variables from NCEP, MODIS and OMI.
 % Solar Energy. xx pp xxx-xxx.
 %
 % ========================================================================
-%                 Raw variables from satellite and NWP 
+%                 Raw variables from satellite and NWP
 % ========================================================================
 %
 %  +---------------------------------------------------------------------+
@@ -39,12 +39,12 @@
 % The user must also specify an overwirte flag, a default assumption is
 % that only unique years will be produced, with exception of the current
 % year, the function will check for new data and then -append the new.
-% 
+%
 % ========================================================================
-%                           Output data sets 
+%                           Output data sets
 % ========================================================================
 % There are many clear-sky irradiance models that can be used. The
-% intention is to provide a dynamic variable for each of their inputs. 
+% intention is to provide a dynamic variable for each of their inputs.
 
 % Angstrom_turbidity_b1     - the Angstrom Turbidity at band 1(beta)
 % Angstrom_turbidity_b2     - the Angstrom Turbidity at band 2(beta)
@@ -76,9 +76,9 @@ setup_nctoolbox
 
 %% Define the directories
 % specify the drive
-store.data_drive_root='F:'; 
+store.data_drive_root='F:';
 % All stores must close with a filesep
-% MODIS store must be a dir that contains MOD08_D3 and MYD08_D3 dirs. 
+% MODIS store must be a dir that contains MOD08_D3 and MYD08_D3 dirs.
 store.MODIS_store=[store.data_drive_root,filesep];
 % AURA store is the dir that contains the OMI_Aura_L3-OMNO2d*.he5 files.
 store.AURA_store=[store.data_drive_root,filesep,'AURA',filesep];
@@ -96,6 +96,11 @@ init_directory(store.raw_outputs_store);
 % not be modified away from 'pressure','relative_humidity', 'temperature',
 % 'aerosol_optical_depth','ozone','nitrogen_dioxide', 'precipitable_water'.
 in_variables={'pressure','relative_humidity','temperature','aerosol_optical_depth','ozone','nitrogen_dioxide','precipitable_water','ground_albedo'};
+% OMI, NCEP, MODIS
+num_of_data_sources=3;
+MODIS_vars={'aerosol_optical_depth','ozone'};
+NCEP_vars={'pressure','temperature','ozone','precipitable_water','relative_humidity'};
+Aura_vars={'ozone','nitrogen_dioxide'};
 
 %% Output variables
 out_variables={'pressure','relative_humidity','temperature','angstrom_turbidity_b1','angstrom_turbidity_b2','angstrom_exponent_b1','angstrom_turbidity_b2','AOD_broadband','AOD_b1','AOD_b2','lambda_b1','lambda_b2','ozone','nitrogen_dioxide','precipitable_water','ground_albedo'};
@@ -109,7 +114,7 @@ end
 % A fundamental component to the main function is the year that is
 % required. The output format of each of the listed output variables is a
 % 3D matrix of longitude*latitude*time for a whole year.
- years=2002:2018;
+years=2002:2018;
 
 %% Specify the overwrite flag
 % The yearly variable files will be overwritten if the flag is set to true,
@@ -117,72 +122,77 @@ end
 % the exception of the current year, whereby new data will be checked for.
 overwrite_flag=true;
 current_year=year(now);
- 
- %% Trigger the main part of the function 
- % This function loops through each of the raw data from the satellite and
- % NWP sources and extracts the usable data that we require.
- % The raw data will then enter a processing stage?
- 
-  for y = 1:length(years)
-     for v = 1:length(in_variables)
- out_variables={'pressure','relative_humidity','temperature','angstrom_turbidity_b1','angstrom_turbidity_b2','angstrom_exponent_b1','angstrom_turbidity_b2','AOD_broadband','AOD_b1','AOD_b2','lambda_b1','lambda_b2','ozone','nitrogen_dioxide','precipitable_water','ground_albedo'};
-        
-         switch in_variables{v}
-             case 'pressure'
-                 %NCEP
-                 pressure=1;
-             case 'relative_humidity'
-                 %NCEP
-                 relative_humidity=1;
-             case 'temperature'
-                 %NCEP
-                 temperature=1;
-             case 'aerosol_optical_depth'
-                 %MODIS
-                 angstrom_turbidity_b1=1;
-                 angstrom_turbidity_b2=1;
-                 angstrom_exponent_b1=1;
-                 angstrom_exponent_b2=1;
-                 AOD_broadband=1;
-                 AOD_b1=1;
-                 AOD_b2=1;
-                 lambda_b1=1;
-                 lambda_b2=1;
-             case 'ozone'
-                 %OMI, MODIS, NCEP
-                 ozone=1;
-             case 'nitrogen_dioxide'
-                 %OMI
-                 nitrogen_dioxide=1;
-             case 'precipitable_water'
-                 %NCEP, MODIS
-                 precipitable_water=1;
-             case 'ground_albedo'
-                 ground_albedo=0.3;
-                 
-             otherwise
-                 error(['Unrecognised variable at y: ',num2str(years(y)),' and v: ',in_variables{v}])
-         end
-         
-         
-     
-     
-     end
-     
-     %% Save the data file
-     for s=1:length(out_variables)
-     filename=[store.raw_outputs_store,filesep,out_variables{s},filesep,out_variables{s},'_',num2str(years(y)),'.mat'];
-     save(filename,out_variables{s});
-     end
-     
- end
 
+%% Trigger the main part of the function
+% This function loops through each of the raw data from the satellite and
+% NWP sources and extracts the usable data that we require.
+% The raw data will then enter a processing stage?
 
-
-
-
-
+for y = 1:length(years)
     
+    % run the calibration
+    [MODIS_raw_process]=ProcessRawDataCalibration(overwrite_flag,current_year,years(y),MODIS_vars,NCEP_vars,OMI_vars,store);
+    
+    raw_year_MODIS=dataExtractionMODIS()
+    
+    
+    
+    raw_year_NCEP=dataExtractionNCEP()
+    raw_year_OMI=dataExtractionOMI()
+    
+    
+    
+    
+    for v=1:length(out_variables)
+        switch out_variables{v}
+            
+            %out_variables={'pressure','relative_humidity','temperature','angstrom_turbidity_b1','angstrom_turbidity_b2','angstrom_exponent_b1','angstrom_turbidity_b2','AOD_broadband','AOD_b1','AOD_b2','lambda_b1','lambda_b2','ozone','nitrogen_dioxide','precipitable_water','ground_albedo'};
+            
+            case 'pressure'
+                %NCEP
+                pressure=1;
+            case 'relative_humidity'
+                %NCEP
+                relative_humidity=1;
+            case 'temperature'
+                %NCEP
+                temperature=1;
+            case 'aerosol_optical_depth'
+                %MODIS
+                angstrom_turbidity_b1=1;
+                angstrom_turbidity_b2=1;
+                angstrom_exponent_b1=1;
+                angstrom_exponent_b2=1;
+                AOD_broadband=1;
+                AOD_b1=1;
+                AOD_b2=1;
+                lambda_b1=1;
+                lambda_b2=1;
+            case 'ozone'
+                %OMI, MODIS, NCEP
+                ozone=1;
+            case 'nitrogen_dioxide'
+                %OMI
+                nitrogen_dioxide=1;
+            case 'precipitable_water'
+                %NCEP, MODIS
+                precipitable_water=1;
+            case 'ground_albedo'
+                ground_albedo=0.3;
+                
+        end
+        
+    end
+    
+    
+    %% Save the data file
+    for s=1:length(out_variables)
+        filename=[store.raw_outputs_store,filesep,out_variables{s},filesep,out_variables{s},'_',num2str(years(y)),'.mat'];
+        
+        save(filename,out_variables{s});
+    end
+    
+end
 
 
 
@@ -198,4 +208,10 @@ current_year=year(now);
 
 
 
- 
+
+
+
+
+
+
+

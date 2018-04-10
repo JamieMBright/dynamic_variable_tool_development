ca695f6a3143fc264039bc6faeb80f3eda4e7772
Jamie Bright <jamie.bright@anu.edu.au>
2018-04-10 11:52:22 +1000

    updted the getFilename to return a confidence filename if the final variable is true

--- support_scripts/GetFilename.m
+++ support_scripts/GetFilename.m
@@ -3,10 +3,22 @@
 % having a single definition so that it can change overtime with different
 % formats.
 
-function filename=GetFilename(store,raw_data_source_var,year,prefix)
+function filename=GetFilename(store,raw_data_source_var,year,prefix,confidence)
 
+% if confidence is not an input
+if ~exist('confidence','var')
+    % default to the assumption that the filename required is not related
+    % to confidence flags
+    confidence=false;
+end
 
-filename=[store.raw_outputs_store,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'.mat'];
-
+% if this is a confidence flag
+if confidence==true
+    % the filename will have confidence within it
+    filename=[store.raw_outputs_store,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'_confidence.mat'];
+else
+    % else it will be titled just as the raw_data_source_var
+    filename=[store.raw_outputs_store,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'.mat'];
+end
 
 end

e069f6e42ab5219e73c80924f6c3d4785a8bb4f8
Jamie Bright <jamie.bright@anu.edu.au>
2018-04-12 14:58:48 +1000

    finished the data assimilation to blend ozone and pwat each year. This is a high memory intensive part of the model requiring up to 10GB memory until a better method is found to handle it.

--- support_scripts/DataAssimilation.m
+++ support_scripts/DataAssimilation.m
@@ -7,6 +7,7 @@
 % their performanve at validation sites.
 
 function DataAssimilation(store,raw_data_source_var,year,prefix_1,prefix_2,weight_1,weight_2)
+disp(['Assimilating data for ',raw_data_source_var,' between ',prefix_1,' and ',prefix_2])
 
 if (~exist('weight_1','var') || ~exist('weight_2','var'))
     weight_1=0.5;
@@ -14,16 +15,48 @@ if (~exist('weight_1','var') || ~exist('weight_2','var'))
 end
 
 % load the necessary data
-data_1=load(GetFilename(store,raw_data_source_var,year,prefix_1));
-data_2=load(GetFilename(store,raw_data_source_var,year,prefix_2));
-confidence_1=load(GetFilename(store,raw_data_source_var,year,prefix_1,'confidence'));
-confidence_2=load(GetFilename(store,raw_data_source_var,year,prefix_2,'confidence'));
-lats_1=load(GetFilename(store,raw_data_source_var,year,prefix_1,'latitudes'));
-lats_2=load(GetFilename(store,raw_data_source_var,year,prefix_2,'latitudes'));
-lons_1=load(GetFilename(store,raw_data_source_var,year,prefix_1,'longitudes'));
-lons_2=load(GetFilename(store,raw_data_source_var,year,prefix_2,'longitudes'));
-times_1=load(GetFilename(store,raw_data_source_var,year,prefix_1,'times_datevec'));
-times_2=load(GetFilename(store,raw_data_source_var,year,prefix_2,'times_datevec'));
+data_1s=load(GetFilename(store,raw_data_source_var,year,prefix_1));
+f=fieldnames(data_1s);
+data_1=eval(['data_1s.',f{1}]);
+clear data_1s
+data_2s=load(GetFilename(store,raw_data_source_var,year,prefix_2));
+f=fieldnames(data_2s);
+data_2=eval(['data_2s.',f{1}]);
+clear data_2s
+confidence_1s=load(GetFilename(store,raw_data_source_var,year,prefix_1,'confidence'));
+f=fieldnames(confidence_1s);
+confidence_1=eval(['confidence_1s.',f{1}]);
+clear confidence_1s
+confidence_2s=load(GetFilename(store,raw_data_source_var,year,prefix_2,'confidence'));
+f=fieldnames(confidence_2s);
+confidence_2=eval(['confidence_2s.',f{1}]);
+clear confidence_2s
+lats_1s=load(GetFilename(store,raw_data_source_var,year,prefix_1,'latitudes'));
+f=fieldnames(lats_1s);
+lats_1=eval(['lats_1s.',f{1}]);
+clear lats_1s
+lats_2s=load(GetFilename(store,raw_data_source_var,year,prefix_2,'latitudes'));
+f=fieldnames(lats_2s);
+lats_2=eval(['lats_2s.',f{1}]);
+clear lats_2s
+lons_1s=load(GetFilename(store,raw_data_source_var,year,prefix_1,'longitudes'));
+f=fieldnames(lons_1s);
+lons_1=eval(['lons_1s.',f{1}]);
+clear lons_1s
+lons_2s=load(GetFilename(store,raw_data_source_var,year,prefix_2,'longitudes'));
+f=fieldnames(lons_2s);
+lons_2=eval(['lons_2s.',f{1}]);
+clear lons_2s
+times_1s=load(GetFilename(store,raw_data_source_var,year,prefix_1,'times_datevec'));
+f=fieldnames(times_1s);
+times_1=eval(['times_1s.',f{1}]);
+times_1=datenum(times_1);
+clear times_1s
+times_2s=load(GetFilename(store,raw_data_source_var,year,prefix_2,'times_datevec'));
+f=fieldnames(times_2s);
+times_2=eval(['times_2s.',f{1}]);
+times_2=datenum(times_2);
+clear times_2s
 
 if weight_1==1
     
@@ -56,11 +89,11 @@ else
     
     
     % find the resolutions of lon, lat and time for both data sets
-    res_y1=lats_1(2)-lats_1(1);
-    res_x1=lons_1(2)-lons_1(1);
+    res_y1=abs(lats_1(2)-lats_1(1));
+    res_x1=abs(lons_1(2)-lons_1(1));
     res_z1=24*(datenum(times_1(2))-datenum(times_1(1)));
-    res_y2=lats_2(2)-lats_2(1);
-    res_x2=lons_2(2)-lons_2(1);
+    res_y2=abs(lats_2(2)-lats_2(1));
+    res_x2=abs(lons_2(2)-lons_2(1));
     res_z2=24*(datenum(times_2(2))-datenum(times_2(1)));
     
     % if the temporal resolutions do not align, then apply maximum weighting to
@@ -103,7 +136,7 @@ else
             % no interpolation needed
             lats=lats_2;
             lons=lons_2;
-           
+            
         elseif res_x1<res_x2
             % then dataset 1 has finer resolution, and dataset2 must be interped
             x=lons_2;
@@ -113,12 +146,20 @@ else
             z=times_1;
             zq=z;
             
-            [X,Y,Z]=meshgrid(x,y,z);
-            [Xq,Yq,Zq]=meshgrid(xq,yq,zq);
+            [X,Y]=meshgrid(x,y);
+            [Xq,Yq]=meshgrid(xq,yq);
             
-            data_2=interp3(X,Y,Z,data_2,Xq,Yq,Zq);
-            confidence_2=interp3(X,Y,Z,confidence_2,Xq,Yq,Zq);
-            clear X Y Z Xq Yq Zq
+            data_2_interped=zeros(size(data_1)).*NaN;
+            confidence_2_interped=zeros(size(data_1)).*NaN;
+            
+            for i=1:length(zq)
+                data_2_interped(:,:,i)=interp2(X,Y,squeeze(data_2(:,:,i)),Xq,Yq);
+                confidence_2_interped(:,:,i)=interp2(X,Y,single(squeeze(confidence_2(:,:,i))),Xq,Yq);
+            end
+            data_2=data_2_interped;
+            confidence_2=confidence_2_interped;
+            confidence_1=double(confidence_1);
+            clear X Y Xq Yq data_2_interped confidence_2_interped
             
             lats=lats_1;
             lons=lons_1;
@@ -131,13 +172,21 @@ else
             yq=lats_2;
             z=times_2;
             zq=z;
-          
-            [X,Y,Z]=meshgrid(x,y,z);
-            [Xq,Yq,Zq]=meshgrid(xq,yq,zq);
             
-            data_1=interp3(X,Y,Z,data_1,Xq,Yq,Zq);
-            confidence_1=interp3(X,Y,Z,confidence_1,Xq,Yq,Zq);
-            clear X Y Z Xq Yq Zq
+            [X,Y]=meshgrid(x,y);
+            [Xq,Yq]=meshgrid(xq,yq);
+            
+            data_1_interped=zeros(size(data_2)).*NaN;
+            confidence_1_interped=zeros(size(data_2)).*NaN;
+            
+            for i=1:length(zq)
+                data_1_interped(:,:,i)=interp2(X,Y,squeeze(data_1(:,:,i)),Xq,Yq);
+                confidence_1_interped(:,:,i)=interp2(X,Y,single(squeeze(confidence_1(:,:,i))),Xq,Yq);
+            end
+            data_1=data_1_interped;
+            confidence_1=confidence_1_interped;
+            confidence_2=confidence_2;
+            clear X Y Xq Yq data_1_interped confidence_1_interped
             
             lats=lats_2;
             lons=lons_2;
@@ -147,29 +196,60 @@ else
         new_data=zeros(size(data_1)).*NaN;
         
         % blend the data using the confidence (1=exist, 0=empty)
-        % introduce the weigthing
+        % pre process the confidence for consistent format. Sometimes NaN
+        % and sometimes 0.
+        confidence_1(isnan(confidence_1))=0;
+        confidence_2(isnan(confidence_2))=0;
+                
         %replace the confidence with the weighting
-        confidence_1(confidence_1==1)=weighting_1;
-        confidence_2(confidence_2==1)=weighting_2;
-        % when there is only 1 value available, that is the value that must
-        % be used
+        confidence_1(confidence_1==1)=weight_1;
+        confidence_2(confidence_2==1)=weight_2;
+        
+        % when there is only 1 value available (e.g. MODIS is available for 
+        % that pixel but NCEP is not), only that value that must be used
+        % and so a weight of 1 must be given.
         confidence_1(confidence_1~=0 & confidence_2==0)=1;
         confidence_2(confidence_2~=0 & confidence_1==0)=1;
-                
+        
+        % As confidence value of 0 is a place holder for NaN values in data
+        % 1 and data 2, we can remove them for simple multiplcation and sum
+        data_1(isnan(data_1))=0;
+        data_2(isnan(data_2))=0;
+        %apply the weigthings stored in the confidence vars to the data 
+        % stored in data vars. then return previous NaNs back to Nan value 
         new_data=data_1.*confidence_1+data_2.*confidence_2;
-        
-        
-        
+        clear data_1 data_2
+        new_data_confidence=confidence_1+confidence_2;
+        clear confidence_1 confidence_2
+        new_data_confidence(new_data_confidence>0)=1;
+        new_data_confidence=int8(new_data_confidence);
+        new_data(new_data_confidence==0)=NaN;
+                
+        % perform the gap filling
         if ~exist('land_mask','var')
-            LAT=reshape(lats,numel(lats),1);
-            LON=reshape(lons,numel(lons),1);
+            [LON,LAT]=meshgrid(lons,lats);
+            LAT=reshape(LAT,numel(LAT),1);
+            LON=reshape(LON,numel(LON),1);
             land_mask=reshape(landmask(LAT,LON),[length(lats),length(lons)]);
         end
         
-        new_data=REST2FillMissing(land_mask,lons,lats,new_data,'true');       
+        new_data=REST2FillMissing(land_mask,lons,lats,new_data,true);
         
         
     end
+    
+    % save the data to file
+    filename=GetFilename(store,raw_data_source_var,year,'blended');
+    save(filename,'new_data')
+    filename=GetFilename(store,raw_data_source_var,year,'blended','confidence');
+    save(filename,'new_data_confidence')
+    filename=GetFilename(store,raw_data_source_var,year,'blended','latitudes');
+    save(filename,'lats')
+    filename=GetFilename(store,raw_data_source_var,year,'blended','longitudes');
+    save(filename,'lons')
+    time=datevec(times_1);
+    filename=GetFilename(store,raw_data_source_var,year,'blended','times_datevec');
+    save(filename,'time')
     
     
     

1807eaf3c84f39dd92be3189b898e94b23038e7e
Jamie Bright <jamie.bright@anu.edu.au>
2018-04-13 14:09:40 +1000

    changed this slightly so that the store input does not necesarily have to be the struct from the dynamic tool, but could also just be a simple string of the storage location of files

--- support_scripts/GetFilename.m
+++ support_scripts/GetFilename.m
@@ -5,6 +5,12 @@
 
 function filename=GetFilename(store,raw_data_source_var,year,prefix,suffix)
 
+if isstruct(store)
+    file_root=store.raw_outputs_store;
+else
+    file_root=store;
+end
+
 % if confidence is not an input
 if ~exist('suffix','var')
     % default to the assumption that the filename required is not related
@@ -16,10 +22,10 @@ end
 % if this is a confidence flag
 if suffix_flag==true
     % the filename will have confidence within it
-    filename=[store.raw_outputs_store,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'_',suffix,'.mat'];
+    filename=[file_root,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'_',suffix,'.mat'];
 else
     % else it will be titled just as the raw_data_source_var
-    filename=[store.raw_outputs_store,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'.mat'];
+    filename=[file_root,raw_data_source_var,filesep,prefix,'_',raw_data_source_var,'_',num2str(year),'.mat'];
 end
 
 end

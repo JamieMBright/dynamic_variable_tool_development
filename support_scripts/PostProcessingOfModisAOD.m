121583408e578a5bb7fcbda433c33fc73bd67b70
Jamie Bright <jamie.bright@anu.edu.au>
2018-04-13 15:47:56 +1000

    added AOD700 derivation and gap filling, also removed some redundant AOD code

--- support_scripts/PostProcessingOfModisAOD.m
+++ support_scripts/PostProcessingOfModisAOD.m
@@ -1,4 +1,4 @@
-function [alpha_b1,alpha_b2,beta_b1,beta_b2,alpha_b1_confidence,alpha_b2_confidence,beta_b1_confidence,beta_b2_confidence]=PostProcessingOfModisAOD(data,latitudes_HDF,longitudes_HDF,land_mask)
+function [alpha_b1,alpha_b2,beta_b1,beta_b2,alpha_b1_confidence,alpha_b2_confidence,beta_b1_confidence,beta_b2_confidence,AOD700,AOD700_confidence]=PostProcessingOfModisAOD(data,latitudes_HDF,longitudes_HDF,land_mask)
 
 % Angstrom Exponent (alpha)
 % Gueymard 2008, Solar Energy 82 272-285, Table 1 states:
@@ -72,7 +72,7 @@ AOD_b2=zeros(size(alpha_b1)).*NaN;
 lambda_b1=zeros(size(alpha_b1)).*NaN;
 lambda_b2=zeros(size(alpha_b1)).*NaN;
 
-% Fill the AOD and lambda bands with the most appropriate AODs
+%% Fill the AOD and lambda bands with the most appropriate AODs
 % BAND1 415 and 674 nm. First fill with available 550, 470, 660 and
 % then 412 if still absent.
 AOD_b1(~isnan(AOD550))=AOD550(~isnan(AOD550));
@@ -91,15 +91,15 @@ AOD700=exp(log(AOD660)-log(700/660).*alpha_b2);
 AOD_b2(isnan(AOD_b2) & ~isnan(AOD700))=AOD700(isnan(AOD_b2) & ~isnan(AOD700));
 lambda_b2(isnan(AOD_b2) & ~isnan(AOD700))=0.7;
 
-% make aconfidence that will indicate raw data
-AOD_b1_confidence=zeros(size(AOD_b1));
-AOD_b1_confidence(~isnan(AOD_b1))=1;
-AOD_b2_confidence=zeros(size(AOD_b2));
-AOD_b2_confidence(~isnan(AOD_b2))=1;
-lambda_b1_confidence=zeros(size(lambda_b1));
-lambda_b1_confidence(~isnan(lambda_b1))=1;
-lambda_b2_confidence=zeros(size(lambda_b2));
-lambda_b2_confidence(~isnan(lambda_b2))=1;
+% % make aconfidence that will indicate raw data
+% AOD_b1_confidence=zeros(size(AOD_b1));
+% AOD_b1_confidence(~isnan(AOD_b1))=1;
+% AOD_b2_confidence=zeros(size(AOD_b2));
+% AOD_b2_confidence(~isnan(AOD_b2))=1;
+% lambda_b1_confidence=zeros(size(lambda_b1));
+% lambda_b1_confidence(~isnan(lambda_b1))=1;
+% lambda_b2_confidence=zeros(size(lambda_b2));
+% lambda_b2_confidence(~isnan(lambda_b2))=1;
 
 % fill missing
 AOD_b1=REST2FillMissing(land_mask,longitudes_HDF,latitudes_HDF,AOD_b1);
@@ -125,30 +125,12 @@ beta_b1(beta_b1>1.1)=1.1;
 beta_b2(beta_b2<0)=0;
 beta_b2(beta_b2>1.1)=1.1;
 
-
-%         %% write these images to file
-%         %make struct
-%         REST2data.latitudes_HDF=latitudes_HDF;
-%         REST2data.longitudes_HDF=longitudes_HDF;
-%         REST2data.alpha_b1=alpha_b1;
-%         REST2data.alpha_b2=alpha_b2;
-%         REST2data.alpha_b1_confidence=alpha_b1_confidence;
-%         REST2data.alpha_b2_confidence=alpha_b2_confidence;
-%         REST2data.beta_b1=beta_b1;
-%         REST2data.beta_b2=beta_b2;
-%         REST2data.beta_b1_confidence=beta_b1_confidence;
-%         REST2data.beta_b2_confidence=beta_b2_confidence;
-%         REST2data.lambda_b1=lambda_b1;
-%         REST2data.lambda_b2=lambda_b2;
-%         REST2data.lambda_b1_confidence=lambda_b1_confidence;
-%         REST2data.lambda_b2_confidence=lambda_b2_confidence;
-%         REST2data.AOD_b1=AOD_b1;
-%         REST2data.AOD_b2=AOD_b2;
-%         REST2data.AOD_b1_confidence=AOD_b1_confidence;
-%         REST2data.AOD_b2_confidence=AOD_b2_confidence;
-%
-%         save(preprocessed_day_filename,'-struct','data')
-%
-%
+%% Derive the AOD700 variable from the remaining values.
+AOD700=exp(log(AOD660)-log(700/660).*alpha_b1);
+AOD700(isnan(AOD700))=exp(log(AOD870(isnan(AOD700)))-log(700/870).*alpha_b2(isnan(AOD700)));
+AOD700(isnan(AOD700))=exp(log(AOD550(isnan(AOD700)))-log(700/550).*alpha_b2(isnan(AOD700)));
+AOD700_confidence=zeros(size(AOD700));
+AOD700_confidence(~isnan(AOD700))=1;
+AOD700=REST2FillMissing(land_mask,longitudes_HDF,latitudes_HDF,AOD700);
 
 end
